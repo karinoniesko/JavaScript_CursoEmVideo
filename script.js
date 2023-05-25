0c6d477010b113a651a595b8a821f85d8ee7c5c4
Karin Oniesko <126200406+karinoniesko@users.noreply.github.com>
2023-05-24 21:02:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function verificar() {
+    var data = new Date()
+    var anohj = data.getFullYear()
+    var txtano = document.querySelector('input#txtano')
+    var ano = Number(txtano.value)
+    var res = document.querySelector('div#res')
+    if (ano.length == 0 || ano > anohj) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var sex = document.getElementsByName('radsex')
+        var idade = anohj - ano
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sex[0].checked) {
+            genero = 'homem'
+            if (idade >= 0 && idade < 14) {
+                //criança
+                img.setAttribute('src', 'assets/2criancam.png')
+            } else if (idade < 30) {
+                //jovem
+                img.setAttribute('src', 'assets/4jovemm.png')
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'assets/6adultom.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'assets/8idosom.png')
+            }
+        } else if (sex[1].checked) {
+            genero = 'mulher'
+            if (idade >= 0 && idade < 14) {
+                //criança
+                img.setAttribute('src', 'assets/1criancaf.png')
+            } else if (idade < 30) {
+                //jovem
+                img.setAttribute('src', 'assets/3jovemf.png')
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'assets/5adultof.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'assets/7idosof.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+    }
+}
